cac2dd4de2d9340132820340fed70f069c538cba
Michal84B <banbura.michal@gmail.com>
2018-03-27 21:02:08 +0200

    Weather Forecast Test Suite

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
@@ -3,7 +3,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class WeatherForecastTestSuite {
-
         @Test
         public void testCalculateForecastWithStub() {
             //Given
