64783ed336c1dcdd6ae620b97f4e9b2f8f6dcda1
Michal84B <banbura.michal@gmail.com>
2018-03-28 17:43:43 +0200

    first test

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculatorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculatorTestSuite.java
@@ -9,7 +9,8 @@ public class CalculatorTestSuite {
     @Before
     public void before() {
         Statistics statisticMock = mock(Statistics.class);
-        Calculator calculator = new Calculator(statisticMock);
+        Calculator calculator = new Calculator();
+        calculator.calculateAdvStatistics(statisticMock);
         List<String> users = new ArrayList<String>();
         for(int i=0; i<100; i++){
             users.add("User Name" +i);
@@ -18,7 +19,12 @@ public class CalculatorTestSuite {
     @Test
     public void testAvgPostsPerUser(){
         //Given
+        when(statisticsMock.username()).thenReturn(users);
+        when(statisticsMock.postCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(0);
         //When
+        double avgPostsPerUser = calculator.getAvgPostsPerUser();
         //Then
+        Assert.assertEquals(10, avgPostsPerUser);
     }
 }
