340473ff6e5f94702c9374ba69b34a57eb6e9f34
Michal84B <banbura.michal@gmail.com>
2018-03-28 17:12:46 +0200

    Calculator Test Suite

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculatorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculatorTestSuite.java
@@ -6,6 +6,19 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CalculatorTestSuite {
+    @Before
+    public void before() {
+        Statistics statisticMock = mock(Statistics.class);
+        Calculator calculator = new Calculator(statisticMock);
+        List<String> users = new ArrayList<String>();
+        for(int i=0; i<100; i++){
+            users.add("User Name" +i);
+        }
+    }
     @Test
-    public void
+    public void testAvgPostsPerUser(){
+        //Given
+        //When
+        //Then
+    }
 }
