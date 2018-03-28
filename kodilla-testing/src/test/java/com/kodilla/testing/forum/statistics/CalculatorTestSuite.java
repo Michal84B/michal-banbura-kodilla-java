719d54e745788fb60adcd171dc6793f280041368
Michal84B <banbura.michal@gmail.com>
2018-03-28 16:34:16 +0200

    Calculator Test Suite

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculatorTestSuite.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.*;
+import java.util.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CalculatorTestSuite {
+    @Test
+    public void
+}
