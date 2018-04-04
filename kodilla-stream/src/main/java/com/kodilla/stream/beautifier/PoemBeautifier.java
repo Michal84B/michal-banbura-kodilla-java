f25198ac1a17b6ffd01acec1eb9a65904cf22bfb
Michal84B <banbura.michal@gmail.com>
2018-04-04 15:37:14 +0200

    added Poem Beautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -1,4 +1,8 @@
-package com.kodilla.stream.beutifier;
+package com.kodilla.stream.beautifier;
 
 public class PoemBeautifier {
+    public void beautify(String x,PoemDecorator poemDecorator){
+        String result = poemDecorator.decorate(x);
+        System.out.println("Some text " + result);
+    }
 }
