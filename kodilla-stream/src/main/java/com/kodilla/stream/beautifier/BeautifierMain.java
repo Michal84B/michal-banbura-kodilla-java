abcfc2cde28499359ec2b4012522941e8fda6e54
Michal84B <banbura.michal@gmail.com>
2018-04-04 15:56:05 +0200

    added Beautifier Main

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/BeautifierMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/BeautifierMain.java
@@ -1,4 +1,14 @@
 package com.kodilla.stream.beautifier;
 
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+
 public class BeautifierMain {
+    public static void main(String[] args){
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Some text to beautify ", x -> "ABC " + x + "ABC");
+        poemBeautifier.beautify("New text", x -> "***" + x + "***");
+        poemBeautifier.beautify("New Text 2", x -> "^^^" + x + "^^^");
+        poemBeautifier.beautify("New Text 3", x -> toUpperCase(x));
+    }
 }
