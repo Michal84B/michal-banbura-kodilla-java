85c19b6bf5595e922b4f5285768d9b1a1682cc4e
Michal84B <banbura.michal@gmail.com>
2018-04-09 20:18:54 +0200

    collector.joining

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,18 +2,17 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
         BookDirectory theBookDirectory = new BookDirectory();
-        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toList());
 
-        System.out.println("# elements: " + theResultListOfBooks.size());
-        theResultListOfBooks.stream()
-                .forEach(System.out::println);
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
+
+        System.out.println(theResultStringOfBooks);
     }
 }
