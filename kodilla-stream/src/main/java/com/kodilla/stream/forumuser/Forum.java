094bae97fd17c52e3e8567574eb1be4a0882c047
Michal84B <banbura.michal@gmail.com>
2018-04-10 20:55:31 +0200

    fixed some bugs

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -8,7 +8,7 @@ public final class Forum {
 
     public Forum() {
         userList.add(new ForumUser(001, "Marek84", 'M',
-                1984, 04, 04, 123));
+                1984, 12, 04, 123));
         userList.add(new ForumUser(002, "Kasia18", 'F',
                 2000, 01,02, 56));
         userList.add(new ForumUser(003,"Grześ15", 'M',
@@ -16,7 +16,7 @@ public final class Forum {
         userList.add(new ForumUser(129, "GrażynaPL", 'F',
                 1980, 07, 25, 203));
         userList.add(new ForumUser(184, "Waldek", 'M',
-                1970, 09, 21, 0));
+                1970, 10, 21, 0));
     }
     public List<ForumUser> getUserList(){
         return new ArrayList<>(userList);
