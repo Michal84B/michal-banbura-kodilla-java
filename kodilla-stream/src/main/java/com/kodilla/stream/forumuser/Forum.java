f24be48de73b52c726bb55da28664b73dc3f34ae
Michal84B <banbura.michal@gmail.com>
2018-04-10 20:14:35 +0200

    Forum with usteList

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -9,5 +9,13 @@ public final class Forum {
     public Forum() {
         userList.add(new ForumUser(001, "Marek84", 'M',
                 1984, 04, 04, 123));
+        userList.add(new ForumUser(002, "Kasia18", 'F',
+                2000, 01,02, 56));
+        userList.add(new ForumUser(003,"Grześ15", 'M',
+                2003, 02, 15, 40));
+        userList.add(new ForumUser(129, "GrażynaPL", 'F',
+                1980, 07, 25, 203));
+        userList.add(new ForumUser(184, "Waldek", 'M',
+                1970, 09, 21, 0));
     }
 }
