4e62a88ef53ce1ffd6673f95e4ef3f588c968c13
Michal84B <banbura.michal@gmail.com>
2018-04-10 20:56:28 +0200

    added first filter

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -1,4 +1,14 @@
 package com.kodilla.stream.forumuser;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamMain {
+    public static void main(String[] args){
+        Forum forum = new Forum();
+
+        Map<Integer, ForumUser> resultUserMap = forum.getUserList().stream()
+                .filter(ForumUser -> ForumUser.getSex() == 'M')
+                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
+    }
 }
