692b89605a32810e2e350256af7c9511c5d88c38
Michal84B <banbura.michal@gmail.com>
2018-04-11 15:44:59 +0200

    Continent

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -1,4 +1,21 @@
-package com.kodilla.stream.World;
+package com.kodilla.stream.world;
 
-public class Continent {
+import java.util.List;
+
+public final class Continent {
+    private final String continentName;
+    private final List<Country> countriesList;
+
+    public Continent(final String continentName, final List<Country> countriesList) {
+        this.continentName = continentName;
+        this.countriesList = countriesList;
+    }
+
+    public String getContinentName() {
+        return continentName;
+    }
+
+    public List<Country> getCountriesList() {
+        return countriesList;
+    }
 }
