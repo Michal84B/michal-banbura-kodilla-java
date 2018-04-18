7cd93ca451d54231d7867b8cbd4ca6109050e65b
Michal84B <banbura.michal@gmail.com>
2018-04-18 17:17:55 +0200

    added try-catch-finally

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -2,6 +2,15 @@ package com.kodilla.exception.test;
 
 public class ExceptionHandling {
     public static void main(String args[]){
+        SecondChallenge secondChallenge = new SecondChallenge();
 
+        try {
+           String result = secondChallenge.probablyIWillThrowException(2, 13);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Error, try again");
+        } finally {
+            System.out.println("'x' must be =< 2 and >1, 'y' can`t be 1.5");
+        }
     }
 }
