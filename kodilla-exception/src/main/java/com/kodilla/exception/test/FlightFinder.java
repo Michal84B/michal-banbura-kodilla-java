b3ae545109c7face7c3e1491a0686e4cb8bacb69
Michal84B <banbura.michal@gmail.com>
2018-04-20 14:27:15 +0200

    added HashMap

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,17 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+
+public class FlightFinder {
+    private HashMap<String, Boolean> airports = new HashMap<String, Boolean>();
+
+    public FlightFinder(HashMap<String, Boolean> airports) {
+        this.airports = airports;
+
+        airports.put("Amsterdam", true);
+        airports.put("Barcelona", true);
+        airports.put("Dublin", false);
+        airports.put("Londyn", false);
+        airports.put("Nicea", true);
+    }
+}
