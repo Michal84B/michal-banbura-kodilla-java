0aefd69e0576cba6a9699e845e5d3c781bdb2e1d
Michal84B <banbura.michal@gmail.com>
2018-04-21 13:07:24 +0200

    fixed findFlight and FlightFinderRunner

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -3,24 +3,17 @@ package com.kodilla.exception.test;
 import java.util.HashMap;
 
 public class FlightFinder {
-    private HashMap<String, Boolean> airports = new HashMap<>();
 
-    public FlightFinder() {
-
+    Boolean findFlight(Flight flight) throws RouteNotFoundException {
+        HashMap<String, Boolean> airports = new HashMap<>();
         airports.put("Amsterdam", true);
         airports.put("Barcelona", true);
         airports.put("Dublin", false);
         airports.put("Londyn", false);
         airports.put("Nicea", true);
-    }
 
-    Boolean findFlight(Flight flight){
-        return airports.get(flight.getArrivalAirport());
-    }
-
-    public Boolean getFlight(String key) throws RouteNotFoundException {
-        if(airports.containsKey(key)) {
-            return airports.get(key);
+        if(airports.containsKey(flight.getArrivalAirport())) {
+            return airports.get(flight);
         }
         else {
             throw new RouteNotFoundException("Airport not found");
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -3,9 +3,10 @@ package com.kodilla.exception.test;
 public class FlightFinderRunner {
     public static void main(String[] args){
         FlightFinder flightFinder = new FlightFinder();
+        Flight flight = new Flight("Warszawa", "Londyn");
 
         try {
-            flightFinder.getFlight("Londyn");
+            flightFinder.findFlight(flight);
         } catch(RouteNotFoundException e) {
             System.out.println("Airport not found, try another airport.");
         }
