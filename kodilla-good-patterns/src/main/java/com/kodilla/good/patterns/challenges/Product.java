7307a74387b3af6dcd3bfb85a2c12159a3397546
Michal84B <banbura.michal@gmail.com>
2018-04-23 21:08:32 +0200

    Product

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Product.java
@@ -0,0 +1,13 @@
+package com.kodilla.good.patterns.challenges;
+
+final class Product {
+    private final String productName;
+
+    public Product(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+}
