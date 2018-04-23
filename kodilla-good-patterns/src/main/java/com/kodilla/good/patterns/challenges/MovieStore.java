7916e152d93478acf92efc589380afd9d54c3bc0
Michal84B <banbura.michal@gmail.com>
2018-04-23 19:05:56 +0200

    added stream

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -1,9 +1,7 @@
 package com.kodilla.good.patterns.challenges;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MovieStore {
     public Map<String, List<String>> getMovies() {
@@ -26,5 +24,15 @@ public class MovieStore {
         booksTitlesWithTranslations.put("FL", flashTranslations);
 
         return booksTitlesWithTranslations;
+    }
+
+    public static void main(String[] args){
+        MovieStore movieStore = new MovieStore();
+
+        String booksTitles = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(booksTitles);
     }
 }
