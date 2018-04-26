d3cceb17c31ed3ae50de79e9f7ffc04059c758cb
Michal84B <banbura.michal@gmail.com>
2018-04-26 21:21:40 +0200

    Product

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/Product.java
@@ -0,0 +1,13 @@
+package com.kodilla.good.patterns.challenges.Food2Door;
+
+public class Product {
+    private final String productName;
+
+    public Product(final String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+}
