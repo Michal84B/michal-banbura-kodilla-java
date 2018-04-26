61e9499736ac6960118a22e0b95f729c06c1a81c
Michal84B <banbura.michal@gmail.com>
2018-04-26 21:38:50 +0200

    ExtraFoodShop

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/ExtraFoodShop.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges.Food2Door;
+
+public class ExtraFoodShop implements Producer {
+
+    @Override
+    public boolean process(Order order) {
+        System.out.println("You can order here some special food")
+        return true;
+    }
+}
