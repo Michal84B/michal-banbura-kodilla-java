452c77e28c7edf337a93a4dc60a3df0181a7e6dd
Michal84B <banbura.michal@gmail.com>
2018-04-26 19:40:04 +0200

    ProductOrderRunner

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderRunner.java
@@ -0,0 +1,12 @@
+package com.kodilla.good.patterns.challenges;
+
+public class ProductOrderRunner {
+    public static void main(String[] args){
+        final User user = new User("JK21", "Jan", "Kowalski");
+        final Product product = new Product("Nokia 6110",156.00);
+        final Order order = new Order(user, product);
+        final OrderProcessor processor = new OrderProcessor(new ProductOrderService());
+        final OrderDto orderDto = processor.process(order);
+
+    }
+}
