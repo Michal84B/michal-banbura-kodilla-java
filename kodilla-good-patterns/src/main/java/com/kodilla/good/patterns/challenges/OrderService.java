54fa0af3857d88ddb9e4049718bcb1a4aaece6a2
Michal84B <banbura.michal@gmail.com>
2018-04-30 10:35:34 +0200

    fixed method name

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderService.java
@@ -2,5 +2,5 @@ package com.kodilla.good.patterns.challenges;
 
 
 public interface OrderService {
-    boolean CreateOrder(User user, Product product);
+    boolean createOrder(User user, Product product);
 }
