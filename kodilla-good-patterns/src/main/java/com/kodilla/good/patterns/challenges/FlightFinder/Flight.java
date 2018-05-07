f61e3eea151db7aab384c5c1f6ce6e62d171f311
Michal84B <banbura.michal@gmail.com>
2018-05-07 16:51:20 +0200

    Flight

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightFinder/Flight.java
@@ -0,0 +1,28 @@
+package com.kodilla.good.patterns.challenges.FlightFinder;
+
+public class Flight {
+
+    private final String departureAirport;
+    private final String arrivalAirport;
+
+    public Flight(final String departureAirport, final String arrivalAirport) {
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "departureAirport='" + departureAirport + '\'' +
+                ", arrivalAirport='" + arrivalAirport + '\'' +
+                '}';
+    }
+}
