c8edd86753f3563148e5c7151fe599766ffd125d
Michal84B <banbura.michal@gmail.com>
2018-05-09 12:55:48 +0200

    Circle

--- kodilla-spring-intro/src/main/java/kodilla/spring/intro/shape/Circle.java
+++ kodilla-spring-intro/src/main/java/kodilla/spring/intro/shape/Circle.java
@@ -1,4 +1,8 @@
 package kodilla.spring.intro.shape;
 
-public class Circle {
+public class Circle extends Figure {
+    @Override
+    public void draw() {
+        System.out.println("This is a circle");
+    }
 }
