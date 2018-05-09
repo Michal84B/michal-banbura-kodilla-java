8bd9d714ca723a8fba261c856e80f674ea295ebc
Michal84B <banbura.michal@gmail.com>
2018-05-09 12:57:45 +0200

    Drawer

---
+++ kodilla-spring-intro/src/main/java/kodilla/spring/intro/shape/Drawer.java
@@ -0,0 +1,11 @@
+package kodilla.spring.intro.shape;
+
+public class Drawer {
+    public void doDrawing() {
+        Circle circle = new Circle();
+        Triangle triangle = new Triangle();
+
+        circle.draw();
+        triangle.draw();
+    }
+}
