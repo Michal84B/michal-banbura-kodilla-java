7c287a01cd08bb79f75770a9032b1e168af33cbe
Michal84B <banbura.michal@gmail.com>
2018-05-09 13:01:43 +0200

    DrawerTestSuite

---
+++ kodilla-spring-intro/src/test/java/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,15 @@
+package kodilla.spring.intro.shape;
+
+import org.junit.Test;
+
+public class DrawerTestSuite {
+    @Test
+    public void testDoDrawing() {
+        //Given
+        Drawer drawer = new Drawer();
+        //When
+        drawer.doDrawing();
+        //Then
+        //do nothing
+    }
+}
