e77eb4c6a45ddca317c7613053195c7b4496cbf7
Michal84B <banbura.michal@gmail.com>
2018-05-11 15:53:39 +0200

    added testConditional

--- kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
@@ -19,4 +19,17 @@ public class ReaderTestSuite {
         //When & Then
         reader.read();
     }
+
+    @Test
+    public void testConditional() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(ReaderConfig.class);
+
+        //When
+        boolean book2Exists = context.containsBean("book2");
+
+        //Then
+        System.out.println("Bean book2 was found in the container: " + book2Exists);
+
+    }
 }
