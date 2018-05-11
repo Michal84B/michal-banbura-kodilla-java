aad515e1ef311f9d2ff4ae237ecee7c770a7b22e
Michal84B <banbura.michal@gmail.com>
2018-05-11 16:05:53 +0200

    TaskList

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private final List<String> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+}
