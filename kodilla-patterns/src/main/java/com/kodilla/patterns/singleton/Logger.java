205be00005759ff089617b53c515a5d120353eaf
Michal84B <banbura.michal@gmail.com>
2018-05-15 16:16:54 +0200

    Singleton

--- kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -1,7 +1,19 @@
 package com.kodilla.patterns.singleton;
 
 public class Logger {
+    private static Logger logger = null;
     private String lastLog = "";
+
+    public static Logger getInstance() {
+        if (logger == null) {
+            synchronized (Logger.class) {
+                if (logger == null) {
+                    logger = new Logger();
+                }
+            }
+        }
+        return logger;
+    }
 
     public void log(String log) {
         lastLog = log;
