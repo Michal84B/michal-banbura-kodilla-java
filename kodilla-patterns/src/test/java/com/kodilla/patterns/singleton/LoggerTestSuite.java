f893b12780bd4f53d3696872a9b4b5db7db0d639
Michal84B <banbura.michal@gmail.com>
2018-05-15 16:27:02 +0200

    Testing singleton

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -5,23 +5,20 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoggerTestSuite {
-    private static Logger logger;
-
     @BeforeClass
     public static void log() {
-        logger = new Logger();
-        logger.log("New Log 1.");
+        Logger.getInstance().log("New singleton log");
     }
 
     @Test
     public void getLastLog() {
         //Given
         //When
-        String logEntry = logger.getLastLog();
+        String logEntry = Logger.getInstance().getLastLog();
         System.out.println("Last log -> " + logEntry);
 
         //Then
-        Assert.assertEquals("New Log 1.", logEntry);
+        Assert.assertEquals("New singleton log", logEntry);
 
     }
 }
