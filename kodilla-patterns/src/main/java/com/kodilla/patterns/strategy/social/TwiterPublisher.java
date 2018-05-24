edcd6ef0231a8a33b0980609f5d928903b009078
Michal84B <banbura.michal@gmail.com>
2018-05-24 15:57:33 +0200

    Twiter

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/TwiterPublisher.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns.strategy.social;
+
+public class TwiterPublisher implements SocialPublisher {
+    @Override
+    public String share() {
+        return "Twiter";
+    }
+}
