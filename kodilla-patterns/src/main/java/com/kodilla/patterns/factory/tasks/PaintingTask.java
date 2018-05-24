c71502111fedcf8de230f8259483137f69c9ff74
Michal84B <banbura.michal@gmail.com>
2018-05-24 23:53:59 +0200

    PaintingTask

--- kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/PaintingTask.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/PaintingTask.java
@@ -1,12 +1,12 @@
 package com.kodilla.patterns.factory.tasks;
 
-public class PaintingTask implements Task {
-    private String taskName;
-    private String color;
-    private String whatToPaint;
+public final class PaintingTask implements Task {
+    private final String taskName;
+    private final String color;
+    private final String whatToPaint;
     private boolean executed;
 
-    public PaintingTask(String taskName, String color, String whatToPaint) {
+    public PaintingTask(final String taskName, final String color, final String whatToPaint) {
         this.taskName = taskName;
         this.color = color;
         this.whatToPaint = whatToPaint;
