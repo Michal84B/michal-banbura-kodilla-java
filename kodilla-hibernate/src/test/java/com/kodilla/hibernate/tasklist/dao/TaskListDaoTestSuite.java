915bfc67ba07c614c6ebed250305bfce53cb3d8b
Michal84B <banbura.michal@gmail.com>
2018-06-02 15:12:25 +0200

    TaskListDaoTestSuite

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String LISTNAME = "To do list";
+
+    @Test
+    public void testFindByListName() {
+
+    }
+}
