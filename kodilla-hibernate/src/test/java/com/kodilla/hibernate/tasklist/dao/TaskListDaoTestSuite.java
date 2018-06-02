6ebbcfdcb5abf061867eb0655c09ee7a963dc3f8
Michal84B <banbura.michal@gmail.com>
2018-06-02 15:30:42 +0200

    FindByListName test

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,10 +1,14 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,9 +16,23 @@ public class TaskListDaoTestSuite {
     @Autowired
     private TaskListDao taskListDao;
     private static final String LISTNAME = "To do list";
+    private static final String DESCRIPTION = "List of tasks to do";
 
     @Test
     public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+        taskListDao.save(taskList);
+        String listname = taskList.getListName();
 
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(listname);
+
+        //Then
+        Assert.assertEquals(1, readTaskList.size());
+
+        //CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListDao.delete(id);
     }
 }
