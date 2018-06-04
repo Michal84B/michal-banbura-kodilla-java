ceb157afd11f3157010baeac30b953a19d6822e3
Michal84B <banbura.michal@gmail.com>
2018-06-04 21:12:35 +0200

    InvoiceDaoTestSuite

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.hibernate.invoice.dao;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+
+        //When
+
+        //Then
+
+        //CleanUp
+    }
+}
